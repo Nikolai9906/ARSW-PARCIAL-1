31ba27d1d87b562e7682c64a843b5dacf038db37
Nikolai9906 <nicohhhh1@gmail.com>
2021-02-25 09:54:22 -0500

    Third point almost finish

--- covidAnalyzerApi/src/main/java/eci/arsw/covidanalyzer/service/ICovidAggregateService.java
+++ covidAnalyzerApi/src/main/java/eci/arsw/covidanalyzer/service/ICovidAggregateService.java
@@ -2,9 +2,12 @@ package eci.arsw.covidanalyzer.service;
 
 import eci.arsw.covidanalyzer.model.Result;
 import eci.arsw.covidanalyzer.model.ResultType;
+import org.springframework.stereotype.Service;
 
+import java.util.List;
 import java.util.UUID;
 
+@Service
 public interface ICovidAggregateService {
 
     /**
@@ -14,7 +17,7 @@ public interface ICovidAggregateService {
      * @param type
      * @return
      */
-    boolean aggregateResult(Result result, ResultType type);
+    void aggregateResult(Result result, ResultType type);
 
     /**
      * Get all the results for the specified result type.
@@ -22,7 +25,7 @@ public interface ICovidAggregateService {
      * @param type
      * @return
      */
-    boolean getResult(ResultType type);
+    List<Result> getResult(ResultType type);
 
     /**
      * 
